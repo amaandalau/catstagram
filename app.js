cb18bd4888c76373ed0433cdf45f6d082500f044
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-04-10 12:15:52 +0800

    Complete Catstagram features

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const catName = document.querySelector(`.cat-name`)
 const catDesc = document.querySelector(`.cat-desc`)
 const gallery = document.querySelector(`.gallery`)
 const profile = document.querySelector(`.cat-profile-btn`)
+const searchInput = document.querySelector(`#meow-search`)
+const usernameText = document.querySelector(`#profile-text`)
 
 let API = `https://api.thecatapi.com/v1/breeds`
+let cats
+let username
 
-async function getCats () {
+async function getCats () { // ✅
     const response = await fetch (API)
     const data = await response.json()
 
@@ -14,7 +18,8 @@ async function getCats () {
     return data
 }
 
-function displayCats(cats) {
+function displayCats(cats) { // ✅
+    console.log(`displayCats:`, cats)
     for (let i=0; i<cats.length; i++) {
         gallery.innerHTML += ` 
         <div class="card-container">
@@ -28,11 +33,60 @@ function displayCats(cats) {
         </div>
         `
     }
+
+    if(cats.length == 0) {
+        alert(`No such cats found`)
+        return
+    }
+}
+
+function searchCats() { // ✅
+    console.log(`cats:`, cats)
+    const searchTerm = searchInput.value.toLowerCase()
+
+    const filteredCats = cats.filter(cat => {
+        return cat.name.toLowerCase().includes(searchTerm)
+      })
+
+      console.log(filteredCats)
+
+    gallery.innerHTML = `` //Clear gallery display before displaying filteredCats
+    displayCats(filteredCats)
+}
+
+function saveUser(username) { // ✅
+    localStorage.setItem('username', username)
+}
+
+function getUser() { // ✅
+    username = localStorage.getItem('username')
+    // console.log(`getUser:`, username)
+
+    if (username) {
+        return username
+    } else {
+        return null
+    }
+}
+
+function displayUsername(username) { // ✅
+    username = getUser()
+
+    if (username) {
+        usernameText.innerText = `Meow, ${username}`
+    } 
 }
 
 window.addEventListener('load', async ()=> {
-    const cats = await getCats()
+    cats = await getCats()
     displayCats(cats)
+
+    if (!username) {
+        const input = prompt('Meow! Please enter your name so I can meowcome you 😹 ')
+        saveUser(input)
+        getUser()
+        displayUsername(input)
+    }
 })
 
-// profile.addEventListener('click', )
+searchInput.addEventListener('input', searchCats)
